7e04eeb7a667290d4b634516bb56afd8dc979239
毛伟 <13639864+maowei0714@user.noreply.gitee.com>
2023-11-25 18:46:52 +0800

    Expense

--- src/pages/Expenses_Manage.tsx
+++ src/pages/Expenses_Manage.tsx
@@ -8,11 +8,11 @@ const schema = {
         {
           "type": "button",
           "actionType": "dialog",
-          "label": "新增费用",
+          "label": "新增入住费用清单",
           "icon": "fa fa-plus pull-left",
           "primary": true,
           "dialog": {
-            "title": "新增费用",
+            "title": "新增入住费用清单",
             "body": {
               "type": "form",
               "name": "sample-edit-form",
@@ -46,19 +46,231 @@ const schema = {
                   "type": "divider"
                 },
                 {
-                  "type": "input-text",
-                  "name": "expenseNmae",
-                  "label": "费用名称",
+                  "type": "select",
+                  "name": "SelectRoom",
+                  "label": "户型类别",
+                  "placeholder": "选择户型类别",
+          "options": [
+            {
+              "label": "标准两人间-1280元每月",
+              "value": "Room_2"
+            },
+            {
+              "label": "标准四人间-1080元每月",
+              "value": "Room_4"
+            },
+            {
+              "label": "套房夫妻间-2400元每月",
+              "value": "House_Couple"
+            },
+            {
+              "label": "套房两人间-1680元每月",
+              "value": "House_2"
+            }
+          ],
                   "required": true
                 },
                 {
+                  "type": "select",
+                  "name": "SelectNursingLevel",
+                  "label": "护理内容",
+                  "placeholder": "选择护理内容（级别）",
+                  "options": [
+                    {
+                      "label": "三级-60元每月",
+                      "value": "NursingLevel_3"
+                    },
+                    {
+                      "label": "二级-120元每月",
+                      "value": "NursingLevel_2"
+                    },
+                    {
+                      "label": "一级-360元每月",
+                      "value": "NursingLevel_1"
+                    },
+                    {
+                      "label": "特级-480元每月",
+                      "value": "NursingLevel_0"
+                    }
+                  ],
+                   "required": true
+
+                },
+                {
                   "type": "divider"
+                },{
+                  "type": "select",
+                  "name": "SelectFaceClean",
+                  "label": "洗脸",
+                  "placeholder": "选择洗脸级别）",
+                  "options": [
+                    {
+                      "label": "协助洗脸-60元每月",
+                      "value": "FaceCleanLevel_3"
+                    },
+                    {
+                      "label": "帮助洗脸-90元每月",
+                      "value": "FaceCleanLevel_2"
+                    },
+                    {
+                      "label": "面部清洁-120元每月",
+                      "value": "FaceCleanLevel_1"
+                    }
+                  ],
+                   "required": true
+
+                },{
+                  "type": "select",
+                  "name": "SelectMouthLevel",
+                  "label": "口腔清洁",
+                  "placeholder": "选择口腔清洁级别",
+                  "options": [
+                   
+                    {
+                      "label": "口腔（假牙）清洁-60元每月",
+                      "value": "MouthLevel_2"
+                    },
+                    {
+                      "label": "口腔护理-120元每月",
+                      "value": "MouthLevel_1"
+                    },
+                    
+                  ],
+                   "required": true
+
+                },
+                {
+                  "type": "select",
+                  "name": "SelectFootLevel",
+                  "label": "洗脚",
+                  "placeholder": "选择洗脚内容",
+                  "options": [
+                   
+                    {
+                      "label": "协助打洗脚水-60元每月",
+                      "value": "FootLevel_2"
+                    },
+                    {
+                      "label": "帮助洗脚洗袜-90元每月",
+                      "value": "FootLevel_1"
+                    },
+                    
+                  ],
+                   "required": true
+
+                },{
+                  "type": "select",
+                  "name": "SelectBathLevel",
+                  "label": "沐浴内容",
+                  "placeholder": "选择沐浴内容",
+                  "options": [
+                    {
+                      "label": "协助沐浴-60元每月",
+                      "value": "NursingLevel_3"
+                    },
+                    {
+                      "label": "帮助沐浴-90元每月",
+                      "value": "NursingLevel_2"
+                    },
+                    {
+                      "label": "擦身沐浴-180元每月",
+                      "value": "NursingLevel_1"
+                    },
+                    
+                  ],
+                   "required": true
+
+                },{
+                  "type": "select",
+                  "name": "SelectCleanLower_Part_Body",
+                  "label": "清洗下身",
+                  "placeholder": "",
+                  "options": [
+                    {
+                      "label": "选择-120元每月",
+                      "value": "Selected"
+                    },
+                    
+                  ],
+                },
+                {
+                  "type": "select",
+                  "name": "SelectCleanClothes",
+                  "label": "清洗衣物",
+                  "placeholder": "",
+                  "options": [
+                    {
+                      "label": "选择-60元每月",
+                      "value": "Selected"
+                    },
+                    
+                  ],
+                },{
+                  "type": "select",
+                  "name": "SelectWearingLevel",
+                  "label": "穿脱衣",
+                  "placeholder": "选择穿拖衣内容",
+                  "options": [
+                    
+                    {
+                      "label": "协助穿脱衣-180元每月",
+                      "value": "WearingLevel_2"
+                    },
+                    {
+                      "label": "帮助穿脱衣-210元每月",
+                      "value": "WearingLevel_1"
+                    },
+                   
+                  ],
+                   "required": true
+
+                },{
+                  "type": "select",
+                  "name": "SelectToiletLevel",
+                  "label": "如厕",
+                  "placeholder": "选择如厕内容",
+                  "options": [
+                    {
+                      "label": "协助入厕-300元每月",
+                      "value": "ToiletLevel_3"
+                    },
+                    {
+                      "label": "大小便清洁-480元每月",
+                      "value": "ToiletLevel_2"
+                    },
+                    {
+                      "label": "大小便护理-600元每月",
+                      "value": "ToiletLevel_1"
+                    },
+                   
+                  ],
+                   "required": true
+
+                },{
+                  "type": "select",
+                  "name": "SelectWheelchairLevel",
+                  "label": "轮椅",
+                  "placeholder": "选择协助轮椅内容",
+                  "options": [
+                    {
+                      "label": "轮椅协助-180元每月",
+                      "value": "WheelchairLevel_2"
+                    },
+                    {
+                      "label": "抱扶轮椅-240元每月",
+                      "value": "WheelchairLevel_1"
+                    },
+                   
+                  ],
+                   "required": true
+
                 },
                 {
                   "type": "input-number",
                   "name": "expenseValue",
                   "label": "费用金额",
-                  "min":"0"
+                  "min":"0",
+                  "required": true
                 },
                 {
                   "type": "divider"
@@ -147,15 +359,15 @@ const schema = {
             "layout": "perPage,pager,go"
           }
         ],
-        "columns": [
+        "columns": [//展示第一列和搜索第一框的属性内容
           {
-            "name": "expenseID",
-            "label": "费用ID",
+            "name": "elderlyID",
+            "label": "老人ID",
             "searchable": {
               "type": "input-text",
-              "name": "expenseID",
-              "label": "费用ID",
-              "placeholder": "输入费用ID"
+              "name": "elderlyID",
+              "label": "老人ID",
+              "placeholder": "输入老人ID"
             },
             "fixed": "left"
           },
@@ -163,14 +375,14 @@ const schema = {
             "name": "elderlyName",
             "label": "老人姓名",
             "fixed": "left",
-            "searchable": true
+            "searchable": {
+              "type": "input-text",
+              "name": "elderlyName",
+              "label": "老人姓名",
+              "placeholder": "输入老人姓名"
+            },
           },
-          {
-            "name": "expenseName",
-            "label": "费用名称",
-            "width": 500,
-            "searchable": true
-          },
+          
           {
             "name": "expenseValue",
             "label": "费用金额",
@@ -179,7 +391,7 @@ const schema = {
           
           {
             "name": "expenseTime",
-            "label": "费用时间"
+            "label": "新增时间"
           },
           {
             "type": "operation",
